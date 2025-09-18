58262c6665b9243fa6faa0413e34f938d9a77d6e
alannasant0s <alannacom2n@gmail.com>
2025-09-18 19:08:19 -0300

    feat: Adiciona versão inicial do consultor de CEP

---
+++ api-cep/projeto_consultor_cep.py
@@ -0,0 +1,33 @@
+import requests
+
+def consulta_enderecos(dados_do_endereco):
+    print("\n--- Endereço Encontrado ---")
+    print(f"Logradouro: {dados_endereco.get('logradouro', 'N/A')}")
+    print(f"Bairro: {dados_endereco.get('bairro', 'N/A')}")
+    print(f"Cidade: {dados_endereco.get('localidade', 'N/A')}")
+    print(f"Estado: {dados_endereco.get('uf', 'N/A')}")
+    print(f"CEP: {dados_endereco.get('cep', 'N/A')}")
+    print("---------------------------\n")
+
+while True:
+    cep_input = input("Bem vindo, insira o cep que deseja consultar: (formato - 00000000)")
+    if cep_input.lower() == 'sair':
+        break
+
+    url = f"https://viacep.com.br/ws/{cep_input}/json/"
+
+    try:
+        resposta = requests.get(url)
+
+        dados_endereco = resposta.json()
+
+        if dados_endereco.get('erro'):
+            print("CEP não encontrado ou inválido. Tente novamente.")
+        else:
+            consulta_enderecos(dados_endereco)
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Erro de conexão: {e}")
+
+print("Fim do programa.")
+
